ea5aa10fc4739971f4bf93fe96bf4a28e61cc801
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-01-02 09:50:38 +0200

    management date from api in page

--- src/Pages/Dashboard/Admin/Home/HomePage.jsx
+++ src/Pages/Dashboard/Admin/Home/HomePage.jsx
@@ -1,80 +1,59 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import CartsOrderSection from "../Orders/AllOrders/CartsOrderSection";
-import { Cart, LoaderLogin } from "../../../../Components/Components";
+import { LoaderLogin } from "../../../../Components/Components";
 import { OrdersComponent } from "../../../../Store/CreateSlices";
 import { useGet } from "../../../../Hooks/useGet";
-// import LineChartComponent from '../../../../Components/Charts/LineChart'
-// import LineChartComponent from '../../../../Components/Charts/LineChart'
-import LineChartComponent from "../../../../Components/Charts/LineChart";
-import Chart from "../../../../Components/Charts/Chart";
-// import { pData,uData,xLabels } from '../../../../Components/Charts/data'
+import Chart from "./Charts/Chart";
 
 const HomePage = () => {
+
+
   const {
     refetch: refetchCountOrders,
     loading,
     data: dataCountOrders,
   } = useGet({
-    url: "https://Bcknd.food2go.online/admin/order/count",
+    url: "https://lamadabcknd.food2go.online/admin/order/count",
   });
 
-  //   const {
-  //     refetch: refetchChart,
-  //     loading: loadingChart,
-  //     data: dataChart,
-  //   } = useGet({
-  //     url: "https://bcknd.food2go.online/admin/home",
-  //   });
-  //   const [dataHome, setDataHome] = useState([]);
-  //   const [order_statistics,setOrder_statistics] = useState({})
-  //   const [earning_statistics,setEarning_statistics] = useState({})
-  //   const [recent_orders,setRecent_orders] = useState([])
+  const {
+    refetch: refetchChart,
+    loading: loadingChart,
+    data: dataCharts,
+  } = useGet({
+    url: "https://lamadabcknd.food2go.online/admin/home",
+  });
+  const [dataHome, setDataHome] = useState([]);
+  const [order_statistics, setOrder_statistics] = useState({})
+  const [earning_statistics, setEarning_statistics] = useState({})
+  const [orders, setOrders] = useState({})
+  const [recent_orders, setRecent_orders] = useState([])
 
-  //   useEffect(() => {
-  //     refetchChart();
-  //   }, [refetchChart]);
-
-
-  //   useEffect(() => {
-  //     if (dataChart) {
-  //       setDataHome(dataChart);
-  //       setOrder_statistics(dataChart.order_statistics)
-  //       setEarning_statistics(dataChart.earning_statistics)
-  //       setRecent_orders(dataChart.recent_orders)
-
-  //     }
-  //     console.log("fetch data Home", dataChart);
-  //     console.log("fetch data Home stat order", dataChart.order_statistics);
-  //     console.log("fetch data Home stat earn",dataChart.earning_statistics);
-  //     console.log("fetch data Home stat recent",dataChart.recent_orders);
-
-  //   }, [dataChart]);
-
-
-  const mockData = {
-    pData: [
-      { x: "Jan", y: 100 },
-      { x: "Feb", y: 150 },
-      { x: "Mar", y: 200 },
-      { x: "Apr", y: 250 },
-      { x: "May", y: 300 },
-    ],
-    uData: [
-      { x: "Jan", y: 80 },
-      { x: "Feb", y: 120 },
-      { x: "Mar", y: 180 },
-      { x: "Apr", y: 220 },
-      { x: "May", y: 280 },
-    ],
-    xLabels: ["Jan", "Feb", "Mar", "Apr", "May"],
-  };
 
   useEffect(() => {
     console.log("Fetching Count Orders...");
     refetchCountOrders();
-  }, [refetchCountOrders]);
+    refetchChart();
+  }, [refetchCountOrders, refetchChart]);
 
+  useEffect(() => {
+    if (dataCharts) {
+      setDataHome(dataCharts);
+      setOrder_statistics(dataCharts.order_statistics)
+      setEarning_statistics(dataCharts.earning_statistics)
+      setRecent_orders(dataCharts.recent_orders)
+      setOrders(dataCharts.orders)
+
+    }
+    console.log("fetch data Home", dataHome);
+    console.log("fetch data Home", dataCharts);
+    console.log("fetch data Home stat order", dataHome.order_statistics);
+    console.log("fetch data Home stat earn", dataHome.earning_statistics);
+    console.log("fetch data Home stat recent", dataHome.recent_orders);
+    console.log("fetch data Home stat order", order_statistics);
+    // console.log("fetch data Home stat order", dataCharts.orders);
+
+  }, [dataCharts, dataHome, order_statistics]);
 
   const counters = {
     ordersAll: dataCountOrders?.orders || 0,
@@ -93,7 +72,7 @@ const HomePage = () => {
     <>
       <OrdersComponent />
       <div className="w-full flex flex-col mb-0">
-        {loading ? (
+        {loading && loadingChart ? (
           <>
             <div className="w-full flex justify-center items-center">
               <LoaderLogin />
@@ -103,7 +82,12 @@ const HomePage = () => {
           <>
             <div className="flex flex-col gap-7 items-start justify-center pb-16">
               <CartsOrderSection ordersNum={counters} />
-              <Chart />
+              <Chart
+                order_statistics={order_statistics}
+                earning_statistics={earning_statistics}
+                recent_orders={recent_orders}
+                orders={orders}
+              />
             </div>
           </>
         )}
