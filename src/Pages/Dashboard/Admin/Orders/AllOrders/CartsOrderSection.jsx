f358339ec02ab93d2362b63969d674c4a9757aba
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-01-02 09:50:41 +0200

    add Charts

--- src/Pages/Dashboard/Admin/Orders/AllOrders/CartsOrderSection.jsx
+++ src/Pages/Dashboard/Admin/Orders/AllOrders/CartsOrderSection.jsx
@@ -11,49 +11,49 @@ const CartsOrderSection = ({ ordersNum }) => {
           route={'/dashboard/orders/pending'}
           icon={<PendingIcon />}
           title={'Pending'}
-          count={ordersNum.ordersPending || 0}
+          count={ordersNum?.ordersPending || 0}
         />
         <Cart
           route={'/dashboard/orders/confirmed'}
           icon={<ConfirmedIcon />}
           title={'Confirmed'}
-          count={ordersNum.ordersConfirmed || 0}
+          count={ordersNum?.ordersConfirmed || 0}
         />
         <Cart
           route={'/dashboard/orders/processing'}
           icon={<ProcessingIcon />}
           title={'Processing'}
-          count={ordersNum.ordersProcessing || 0}
+          count={ordersNum?.ordersProcessing || 0}
         />
         <Cart
           route={'/dashboard/orders/out_for_delivery'}
           icon={<OutForDeliveryIcon />}
           title={'Out For Delivery'}
-          count={ordersNum.OutForDelivery || 0}
+          count={ordersNum?.OutForDelivery || 0}
         />
         <Cart
           route={'/dashboard/orders/delivered'}
           icon={<DeliveredIcon />}
           title={'Delivered'}
-          count={ordersNum.ordersDelivered || 0}
+          count={ordersNum?.ordersDelivered || 0}
         />
         <Cart
           route={'/dashboard/orders/canceled'}
           icon={<CanceledIcon />}
           title={'Canceled'}
-          count={ordersNum.ordersCanceled || 0}
+          count={ordersNum?.ordersCanceled || 0}
         />
         <Cart
           route={'/dashboard/orders/returned'}
           icon={<ReturnedIcon />}
           title={'Returned'}
-          count={ordersNum.ordersReturned || 0}
+          count={ordersNum?.ordersReturned || 0}
         />
         <Cart
           route={'/dashboard/orders/failed_to_deliver'}
           icon={<FailedToDeliverIcon />}
           title={'Failed To Deliver'}
-          count={ordersNum.FailedToDeliver || 0}
+          count={ordersNum?.FailedToDeliver || 0}
         />
       </div>
     </>
